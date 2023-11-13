2a94c73c5f2e12a84876548f36e8a98bf565fcbc
raspicor <crayon3371@naver.com>
2023-11-13 11:34:36 +0900

    Detail Changed

--- lib/presentation/chat_view.dart
+++ lib/presentation/chat_view.dart
@@ -12,57 +12,64 @@ class _ChatViewState extends State<ChatView> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Chat with WS',
-      home: Scaffold(appBar: AppBar(
-        leading: IconButton(icon: const Icon(Icons.refresh), onPressed: () => {},),
-      ),
-      body: Column(
-        children: [
-          const Expanded(
-            flex:8,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child : Expanded(
-                flex: 5,
-                child: Column(
-                  children: [
-                    Text('test', style: TextStyle(fontSize: 40),),
-                  ],
-                ),
-              ),
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () => {},
           ),
-          Expanded(child: Row(
-            children: [
-              Expanded(
-                flex: 10,
-                child: Container(
-                  margin: const EdgeInsets.all(10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      icon: const Icon(Icons.message),
-                      hintText: 'Input Your Messages',
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(color : Colors.blue, width: 3),
-                      )
-                    ),
+        ),
+        body: Column(
+          children: [
+            const Expanded(
+              flex: 8,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Expanded(
+                  flex: 5,
+                  child: Column(
+                    children: [
+                      Text(
+                        'test',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: IconButton(onPressed: ()=>{}, icon: const Icon(Icons.send)),
-              )
-            ],
-          ))
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  flex: 10,
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          icon: const Icon(Icons.message),
+                          hintText: 'Input Your Messages',
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                                const BorderSide(color: Colors.blue, width: 3),
+                          )),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: IconButton(
+                      onPressed: () => {}, icon: const Icon(Icons.send)),
+                )
+              ],
+            ))
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: const [
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
           BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
-        ]
-      ),
+        ]),
       ),
     );
   }
